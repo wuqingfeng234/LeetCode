6e54c257185e2c4b667a9b5f141d00a05ed32124
wuqingfeng <wuqingfeng234@163.com>
2023-09-03 20:05:43 +0800

    linked list middle

---
+++ src/main/java/com/git/wuqf/Solution876.java
@@ -0,0 +1,35 @@
+package com.git.wuqf;
+
+import java.util.List;
+
+public class Solution876 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+//        l5.next = l6;
+        Solution876 solution876 = new Solution876();
+        ListNode listNode = solution876.middleNode(l1);
+        System.out.println("x");
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast.next != null) {
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
