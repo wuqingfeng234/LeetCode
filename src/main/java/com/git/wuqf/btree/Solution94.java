656aed7fc43fad81991af081d404b07d22c523d3
wuqingfeng <wuqingfeng234@163.com>
2023-09-09 13:05:57 +0800

    Traversing  b tree in in order

--- src/main/java/com/git/wuqf/btree/Solution94.java
+++ src/main/java/com/git/wuqf/btree/Solution94.java
@@ -2,6 +2,7 @@ package com.git.wuqf.btree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution94 {
 
@@ -12,20 +13,27 @@ public class Solution94 {
         t1.left = null;
         t1.right = t2;
         t2.left = t3;
-        Solution94 solution94=new Solution94();
+        Solution94 solution94 = new Solution94();
         List<Integer> integers = solution94.inorderTraversal(t3);
         System.out.println("x");
     }
 
-    List<Integer> ans = new ArrayList<>();
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-        if (root == null) {
-            return ans;
+    public List<Integer> inorderTraversal(TreeNode cur) {
+        List<Integer> ans = new ArrayList<>();
+        if (cur != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            while (!stack.isEmpty() || cur != null) {
+                if (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                } else {
+                    cur = stack.pop();
+                    ans.add(cur.val);
+                    cur=cur.right;
+                }
+            }
         }
-        inorderTraversal(root.left);
-        ans.add(root.val);
-        inorderTraversal(root.right);
         return ans;
     }
 }
