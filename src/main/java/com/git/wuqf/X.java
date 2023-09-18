c56df2c21b6b2cc5b52fd2b9b8f256d8876fee18
wuqingfeng <wuqingfeng234@163.com>
2023-09-18 20:10:01 +0800

    for and --

---
+++ src/main/java/com/git/wuqf/X.java
@@ -0,0 +1,13 @@
+package com.git.wuqf;
+
+public class X {
+    public static void main(String[] args) {
+        int[] x = new int[10];
+        for (int i = 0; i < 10; i++) {
+            x[i] = i;
+        }
+        for (int j = 9; j >= 0; j--) {
+            System.out.println(j);
+        }
+    }
+}
