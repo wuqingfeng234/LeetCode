2738cf0701b6a31632a78472b03bf5d63ef3662a
wuqingfeng <wuqingfeng234@163.com>
2023-10-14 17:54:21 +0800

    solution 239

--- src/main/java/com/git/wuqf/Solution239.java
+++ src/main/java/com/git/wuqf/Solution239.java
@@ -1,31 +1,38 @@
 package com.git.wuqf;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Solution239 {
     public static void main(String[] args) {
         int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
         int k = 3;
-        Solution239 s=new Solution239();
+        Solution239 s = new Solution239();
         int[] ints = s.maxSlidingWindow(nums, k);
         System.out.println(ints);
     }
 
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int left = 0;
-        int right = left + k-1;
-        int[] ms = new int[nums.length - k + 1];
-        for (int i = 0; right < nums.length; left++, right++, i++) {
-            ms[i] = findMax(nums, left, right);
+        int[] max = new int[nums.length - k + 1];
+        Deque<Integer> window = new LinkedList<>();
+        for (int i = 0; i < k - 1; i++) {
+            addWindow(window, nums, i, k);
         }
-        return ms;
-    }
 
-    private int findMax(int[] nums, int left, int right) {
-        int max = nums[left];
-        for (int i = left + 1; i <= right; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
-            }
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            addWindow(window, nums, i + k - 1, k);
+            max[i] = nums[window.peek()];
         }
         return max;
+    }
+
+    private void addWindow(Deque<Integer> window, int[] nums, int index, int limit) {
+        while (!window.isEmpty() && nums[window.peekLast()] <= nums[index]) {
+            window.pollLast();
+        }
+        window.offer(index);
+        while ((index - window.peek() + 1) > limit) {
+            window.poll();
+        }
     }
 }
